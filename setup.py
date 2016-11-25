c417a9205b9da9ec1b77ae42fdc514b511befd48
Francisco Ramirez <franchuti688@gmail.com>
2016-11-25 15:30:30 +0100

    Added setup.py and version hardcoded to develop

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+REQUIREMENTS = open(os.path.join(here, 'requirements', 'common.txt')).readlines()
+
+from modelmapper import __version__
+
+
+setup(
+    name='modelmapper',
+    version=__version__,
+    author='Taric S.A.',
+    author_email='desarrollo.madrid@taric.es',
+    maintainer='Desarrollo Taric',
+    maintainer_email='desarrollo.madrid@taric.es',
+    long_description=README,
+    description='Gestor de conexiones entre estructuras de datos',
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+)
