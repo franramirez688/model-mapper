71b199231d1359c11d3802d4162f20fc015dcd97
Francisco Ramirez <franchuti688@gmail.com>
2017-02-02 17:32:30 +0100

    Deleted bad 'meta_mapper' attribute (useless)

--- modelmapper/core.py
+++ modelmapper/core.py
@@ -20,7 +20,7 @@ class ModelMapper(object):
     """Linker class between an origin model and a destination one
     """
 
-    def __init__(self, origin_model, destination_model, mapper, meta_mapper=None):
+    def __init__(self, origin_model, destination_model, mapper):
         self._origin_model = origin_model if origin_model is None else dict()
         self._destination_model = destination_model
         self._mapper = mapper  # MutableMapping()
@@ -28,9 +28,6 @@ class ModelMapper(object):
         self._mapper_accessor = ModelDictAccessor(mapper)
         self._origin_accessor = ModelAccessor(origin_model)
         self._destination_accessor = ModelAccessor(destination_model)
-
-        self._meta_mapper = meta_mapper
-        self._meta_mapper_accessor = ModelDictAccessor(meta_mapper)
 
         # Variable to record tuple(orig_access, dest_access, model_mapper_obj)
         self._children_declarations = set()
@@ -171,13 +168,13 @@ class ModelMapper(object):
 
 class UniformListModelMapper(ModelMapper):
 
-    def __init__(self, origin_model, destination_model, mapper, meta_mapper=None):
+    def __init__(self, origin_model, destination_model, mapper):
         assert isinstance(origin_model, list), "Origin model must be a list with uniform data"
 
         self._orig_data = origin_model or []
         self._index = 0
         origin_model = origin_model[0] if len(origin_model) > 0 else dict()
-        super(UniformListModelMapper, self).__init__(origin_model, destination_model, mapper, meta_mapper=meta_mapper)
+        super(UniformListModelMapper, self).__init__(origin_model, destination_model, mapper)
 
     @property
     def orig_data(self):
@@ -261,9 +258,9 @@ class ListModelMapper(ModelMapper):
 
     LINK = '[{}].{}'
 
-    def __init__(self, origin_model, destination_model, mapper, meta_mapper=None):
+    def __init__(self, origin_model, destination_model, mapper):
         origin_model = origin_model if origin_model is None else list()
-        super(ListModelMapper, self).__init__(origin_model, destination_model, mapper, meta_mapper=meta_mapper)
+        super(ListModelMapper, self).__init__(origin_model, destination_model, mapper)
 
     def _values_updater(self, func_name, accessor_to_set, accessor_to_get, setter_index, getter_index):
         link = ListModelMapper.LINK.format
